9afea54ee655008b34e4a74bdb6c81eeb6f6ff21
Matthew <misayan@gmail.com>
2023-03-22 00:06:32 -0700

    Structure + Theme Update

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1',
+    VERSION: '1.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
+    BUILDER: 'singlehtml',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
